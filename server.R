e007ce3afd75cffecea8e7480a2ccf0c3cff12f2
Adam Starke <afstarke@gmail.com>
2014-09-09 22:37:04 -0400

    move functions to proper area

--- server.R
+++ server.R
@@ -15,60 +15,61 @@ library(shiny)
 
 
 # Define each part of the loading formula
-#a
-AtmNatVeg <- function(){
-	return(input$AtmDepRate * input$NatVegArea * input$NtransNatVeg)
-}
-#b
-AtmTurf <- function(){
-	return(input$AtmDepRate * input$TurfArea * input$TransTurf)
-}
-#c
-AtmAg <- function(){
-	return(input$AtmDepRate * input$AgArea * input$NTransAg)
-}
-#d
-AtmImperv <- function(){
-	return((input$AtmDepRate * (input$RoofArea + input$DrivewayArea) * input$NtransTurf) * (input$AtmDepRate * input$ImpervArea)) #Need help with this formula
-}
-#e
-AtmWetlands <- function(){
-	return(input$AtmDepRate * input$WetlandsArea)
-}
-#f
-AtmPonds <- function(){
-	return(input$AtmDepRate * input$PondsArea * input$ThroughAquiferPonds)
-}
-## Total N load to estuary sourced from Atmospheric Deposition
-TotalLoadAtmospheric <- function(){
-	return(AtmNatVeg() + AtmTurf() + AtmAg() + AtmImperv() + AtmWetlands() + AtmPonds())
-}
-
-
-
-##Via fertilizer application
-
-#g
-FertTurf <- function(){
-	return(input$FertLawns * input$LawnArea * input$PercentHomes * input$DeNitTurf)
-}
-#h 	
-FertAg <- function(){
-	return(input$FertAg * input$AgArea * input$DeNit)
-}
-#i
-FertGolf <- function(){
-	return(input$Fert)
-}
-
 
 
 
 shinyServer( # this will be run each time a user changes something.
 	function(input, output) {
+		#a
+		AtmNatVeg <- function(){
+			return(input$AtmDepRate * input$NatVegArea * input$NtransNatVeg)
+		}
+		#b
+		AtmTurf <- function(){
+			return(input$AtmDepRate * input$TurfArea * input$TransTurf)
+		}
+		#c
+		AtmAg <- function(){
+			return(input$AtmDepRate * input$AgArea * input$NTransAg)
+		}
+		#d
+		AtmImperv <- function(){
+			return((input$AtmDepRate * (input$RoofArea + input$DrivewayArea) * input$NtransTurf) * (input$AtmDepRate * input$ImpervArea)) #Need help with this formula
+		}
+		#e
+		AtmWetlands <- function(){
+			return(input$AtmDepRate * input$WetlandsArea)
+		}
+		#f
+		AtmPonds <- function(){
+			return(input$AtmDepRate * input$PondsArea * input$ThroughAquiferPonds)
+		}
+	
+		## Total N load to estuary sourced from Atmospheric Deposition
+		TotalLoadAtmospheric <- function(){
+			return(AtmNatVeg() + AtmTurf() + AtmAg() + AtmImperv() + AtmWetlands() + AtmPonds())
+		}
+		
+		
+		
+		##Via fertilizer application
+		
+		#g
+		FertTurf <- function(){
+			return(input$FertLawns * input$LawnArea * input$PercentHomes * input$DeNitTurf)
+		}
+		#h 	
+		FertAg <- function(){
+			return(input$FertAg * input$AgArea * input$DeNit)
+		}
+		#i
+		FertGolf <- function(){
+			return(input$Fert)
+		}
+		
 
 	output$Load1 <- renderText({
-		paste("Your total load is ", AtmNatVeg())
+		paste("Your total load is ", AtmNatVeg() + AtmTurf())
 	})
   }
  )
