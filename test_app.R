fe79bf7b2a5191c9a9018ba4c2818956bfb9635b
Brewstarke <afstarke@gmail.com>
2014-10-30 08:57:16 -0400

    ignore

---
+++ test_app.R
@@ -0,0 +1,12 @@
+
+# in the app this data will be loaded in using fileInput
+data <- data.frame(x= 1:30, y = 30:1)
+
+
+shinyServer( # this will be run each time a user changes something.
+	function(input, output) {
+	
+		
+		
+	}
+)
